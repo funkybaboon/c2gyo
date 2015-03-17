7c1fa9290f6df122ef438044bf4583233fc255aa
funkybaboon <funkybaboon@gmail.com>
2015-03-17 20:09:19 +0100

    Added a reset button

--- app/scripts/directives/timeinputform.js
+++ app/scripts/directives/timeinputform.js
@@ -24,6 +24,23 @@ angular.module('c2gyoApp')
           this.rental.tab = setTab;
         };
 
+        $scope.clear = function() {
+          var now = new moment().startOf('hour').add(1, 'h');
+
+          $scope.rental = {
+            tab: $scope.rental.tab,
+            startDate: now.clone(),
+            endDate: now.clone(),
+            distance: 0,
+            timeMinutes: 0,
+            timeHours: 0,
+            timeDays: 0,
+            timeWeeks: 0,
+            timeStanding: 0,
+            airport: false
+          };
+        };
+
       }
     };
   });
