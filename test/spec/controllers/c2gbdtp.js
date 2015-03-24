d1d88d9f5306c9a50c0cbe9313f2473e99e42870
funkybaboon <funkybaboon@gmail.com>
2015-03-24 15:16:31 +0100

    fixed typo

--- test/spec/controllers/c2gbdtp.js
+++ test/spec/controllers/c2gbdtp.js
@@ -72,50 +72,50 @@ describe('Controller: C2gbdtpCtrl', function() {
     var now = new moment();
     scope.rental.tab = 'exact';
 
-    scope.rental.startDate = now.clone().startOf('hour').add(1, 'h'),
-    scope.rental.endDate = now.clone().startOf('hour').add(2, 'h'),
+    scope.rental.startDate = now.clone().startOf('hour').add(1, 'h');
+    scope.rental.endDate = now.clone().startOf('hour').add(2, 'h');
     scope.rental.distance = 10;
     scope.rental.airport = false;
     expect(scope.price().toFixed(2)).toEqual((14.90).toFixed(2));
 
-    scope.rental.startDate = now.clone().startOf('hour').add(1, 'h'),
-    scope.rental.endDate = now.clone().startOf('hour').add(2, 'h'),
+    scope.rental.startDate = now.clone().startOf('hour').add(1, 'h');
+    scope.rental.endDate = now.clone().startOf('hour').add(2, 'h');
     scope.rental.distance = 10;
     scope.rental.airport = true;
     expect(scope.price().toFixed(2)).toEqual((19.80).toFixed(2));
 
-    scope.rental.startDate = now.clone().startOf('hour').add(1, 'h'),
-    scope.rental.endDate = now.clone().startOf('hour').add(34, 'h'),
+    scope.rental.startDate = now.clone().startOf('hour').add(1, 'h');
+    scope.rental.endDate = now.clone().startOf('hour').add(34, 'h');
     scope.rental.distance = 10;
     scope.rental.airport = false;
     expect(scope.price().toFixed(2)).toEqual((178.00).toFixed(2));
 
-    scope.rental.startDate = now.clone().startOf('hour').add(1, 'h'),
-    scope.rental.endDate = now.clone().startOf('hour').add(34, 'h'),
+    scope.rental.startDate = now.clone().startOf('hour').add(1, 'h');
+    scope.rental.endDate = now.clone().startOf('hour').add(34, 'h');
     scope.rental.distance = 10;
     scope.rental.airport = true;
     expect(scope.price().toFixed(2)).toEqual((182.90).toFixed(2));
 
-    scope.rental.startDate = now.clone().startOf('hour').add(1, 'h'),
-    scope.rental.endDate = now.clone().startOf('hour').add(2, 'h'),
+    scope.rental.startDate = now.clone().startOf('hour').add(1, 'h');
+    scope.rental.endDate = now.clone().startOf('hour').add(2, 'h');
     scope.rental.distance = 100;
     scope.rental.airport = false;
     expect(scope.price().toFixed(2)).toEqual((29.40).toFixed(2));
 
-    scope.rental.startDate = now.clone().startOf('hour').add(1, 'h'),
-    scope.rental.endDate = now.clone().startOf('hour').add(2, 'h'),
+    scope.rental.startDate = now.clone().startOf('hour').add(1, 'h');
+    scope.rental.endDate = now.clone().startOf('hour').add(2, 'h');
     scope.rental.distance = 100;
     scope.rental.airport = true;
     expect(scope.price().toFixed(2)).toEqual((34.30).toFixed(2));
 
-    scope.rental.startDate = now.clone().startOf('hour').add(1, 'h'),
-    scope.rental.endDate = now.clone().startOf('hour').add(34, 'h'),
+    scope.rental.startDate = now.clone().startOf('hour').add(1, 'h');
+    scope.rental.endDate = now.clone().startOf('hour').add(34, 'h');
     scope.rental.distance = 1000;
     scope.rental.airport = false;
     expect(scope.price().toFixed(2)).toEqual((453.50).toFixed(2));
 
-    scope.rental.startDate = now.clone().startOf('hour').add(1, 'h'),
-    scope.rental.endDate = now.clone().startOf('hour').add(34, 'h'),
+    scope.rental.startDate = now.clone().startOf('hour').add(1, 'h');
+    scope.rental.endDate = now.clone().startOf('hour').add(34, 'h');
     scope.rental.distance = 1000;
     scope.rental.airport = true;
     expect(scope.price().toFixed(2)).toEqual((458.40).toFixed(2));
