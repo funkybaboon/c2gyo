b24fbeb45a0684689de0d91e092677d60e0f91d6
Benjamin <funkybaboon@gmail.com>
2015-04-23 23:18:14 +0200

    refactored tests into loop

--- test/spec/controllers/flinkster.js
+++ test/spec/controllers/flinkster.js
@@ -17,48 +17,55 @@ describe('Controller: FlinksterCtrl', function () {
     flinksterConfig = _flinksterConfig_;
   }));
 
-  var testdata = {
-    bundesweit: {
-      sonder: '26.00',
-      mini: '24.00',
-      klein: '48.30',
-      kompakt: '57.70',
-      mittel: '66.90',
-      transporter: '75.90'
-    },
-    lokal: {
-      sonder: '18.35',
-      mini: '18.35',
-      klein: '23.85',
-      kompakt: '28.35',
-      mittel: '42.40',
-      transporter: '42.40'
-    }
-  };
 
-  var testcase = function (tariff, carClass, expectedPrice) {
-    it('distance: ' + 10 + ', tariff: ' + tariff + ', carClass: ' + carClass, function () {
+  var test = function( testdata ) {
+    for (var tariff in testdata.expectedPrices) {
+      for (var carClass in testdata.expectedPrices[tariff]) {
+        var expectedPrice = testdata.expectedPrices[tariff][carClass];
+        it('tariff: ' + tariff + ', carClass: ' + carClass, function () {
+          scope.rental.startDate = new moment().startOf('hour').hour(7).isoWeekday(1);
+          scope.rental.endDate = scope.rental.startDate.clone().add(10, 'h');
 
-      scope.rental.startDate = new moment().startOf('hour').hour(7).isoWeekday(1);
-      scope.rental.endDate = scope.rental.startDate.clone().add(10, 'h');
+          scope.rental.tab = testdata.tab;
+          scope.rental.distance = testdata.distance;
 
-      scope.rental.tab = 'exact';
-      scope.rental.distance = 10;
-
-      scope.rate.tariff = tariff;
-      scope.rate.carClass = carClass;
-      expect(scope.price().toFixed(2)).toEqual(expectedPrice);
-    });
-  };
-
-  for (var tariff in testdata) {
-    for (var carClass in testdata[tariff]) {
-      var expectedPrice = testdata[tariff][carClass];
-      testcase(tariff, carClass, expectedPrice);
+          scope.rate.tariff = tariff;
+          scope.rate.carClass = carClass;
+          expect(scope.price().toFixed(2)).toEqual(expectedPrice);
+        });
+      }
     }
   }
 
+  describe('should calculate the correct price with 10km distance and 10 hour lease', function () {
 
+    var testdata = {
+      tab: 'exact',
+      distance: 10,
+      expectedPrices: {
+        bundesweit: {
+          sonder: '26.00',
+          mini: '24.00',
+          klein: '48.30',
+          kompakt: '57.70',
+          mittel: '66.90',
+          transporter: '75.90'
+        },
+        lokal: {
+          sonder: '18.35',
+          mini: '18.35',
+          klein: '23.85',
+          kompakt: '28.35',
+          mittel: '42.40',
+          transporter: '42.40'
+        }
+      }
+    };
+
+    test(testdata);
+
+  })
+/*
   it('should calculate the correct price using the datetimepicker', function() {
     scope.rental.startDate = new moment().startOf('hour').hour(7).isoWeekday(1);
     scope.rental.endDate = scope.rental.startDate.clone().add(10, 'h');
@@ -107,6 +114,6 @@ describe('Controller: FlinksterCtrl', function () {
     expect(scope.price().toFixed(2)).toEqual((42.40).toFixed(2));
 
   });
-
+*/
 
 });
