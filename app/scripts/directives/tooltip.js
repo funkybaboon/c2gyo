10d26a82682070bf6958750c3cbdc305512859bb
Benjamin <funkybaboon@gmail.com>
2015-06-16 08:10:18 +0200

    add tooltip directive

---
+++ app/scripts/directives/tooltip.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/**
+ * @ngdoc directive
+ * @name c2gyoApp.directive:tooltip
+ * @description
+ * # tooltip
+ */
+angular.module('c2gyoApp')
+  .directive('tooltip', function () {
+    return {
+      template: '<div></div>',
+      restrict: 'A',
+      link: function postLink(scope, element, attrs) {
+        element.text('this is the tooltip directive');
+      }
+    };
+  });
