428005908c71404bd6bc5b818c8bd6c75871b671
Benjamin <funkybaboon@gmail.com>
2015-06-25 08:33:20 +0200

    add text to sm car class popovers

--- app/scripts/services/smconfig.js
+++ app/scripts/services/smconfig.js
@@ -10,5 +10,17 @@
 angular.module('c2gyoApp')
   .constant('smConfig', {
     tariff: 'classic',
-    carClass: 'A'
+    carClass: 'A',
+    info: {
+      carClass: {
+        rateA: 'Toyota Aygo',
+        rateB: 'Opel Agila, Opel Corsa, Renault Zoe, Toyota Yaris Hybrid, ' +
+        'Fiat 500 Cabrio, Opel Adam, Renault Wind, Opel Combo Kastenwagen, ' +
+        'Renault Kangoo Kastenwagen',
+        rateC: 'Opel Astra Kombi, Ford Tourneo Hochdachkombi, Mini One',
+        rateD: 'BMW 116d, Mazda MX5 Cabrio, Ford Custom Kleinbus, ' +
+        'Opel Vivaro Kleinbus',
+        rateF: 'Ford Transit Transporter'
+      }
+    }
   });
