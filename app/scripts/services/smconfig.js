a9d66a58b11244c34269cb26aa2bf8fe9ce25e27
Benjamin <funkybaboon@gmail.com>
2015-06-26 08:41:58 +0200

    added popover text for sm tariffs

--- app/scripts/services/smconfig.js
+++ app/scripts/services/smconfig.js
@@ -21,6 +21,14 @@ angular.module('c2gyoApp')
         rateD: 'BMW 116d, Mazda MX5 Cabrio, Ford Custom Kleinbus, ' +
         'Opel Vivaro Kleinbus',
         rateF: 'Ford Transit Transporter'
+      },
+      tariff: {
+        tariffbasic: '4€ pro Person, 4€ pro weiterer Person, ohne Kaution',
+        tariffclassic: '11€ pro Person, 15€ pro Haushalt, ohne Kaution. ' +
+        '7,50€ pro Person, 11€ pro Haushalt, 550€ Kaution.',
+        tariffstudi: '5€ pro Person, 200€ Kaution. Tarif entspricht Classic ' +
+        'Tarif',
+        tariffbusiness: '29€ pro Haushalt, 550€ Kaution'
       }
     }
   });
