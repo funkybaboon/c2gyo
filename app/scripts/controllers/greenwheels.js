a0fc774bd00248d3406f2932ea8f949a3a71fc84
Benjamin <funkybaboon@gmail.com>
2015-08-06 06:12:27 +0200

    Add greenwheels controller

--- app/scripts/controllers/greenwheels.js
+++ app/scripts/controllers/greenwheels.js
@@ -10,25 +10,28 @@
 angular.module('c2gyoApp')
   .controller('GreenwheelsCtrl', [
     '$scope',
-    'c2gConfig',
+    'greenwheelsrate',
+    'greenwheelsConfig',
     'duration',
     'state',
-    function($scope, c2gConfig, duration, state) {
+    function(
+      $scope,
+      greenwheelsrate,
+      greenwheelsConfig,
+      duration,
+      state) {
+
       $scope.rental = state.rental;
       $scope.clear = function() {
         state.clearRental($scope.rental.tab);
       };
 
-      $scope.vendor = c2gConfig.vendor;
+      $scope.rate = {
+        carClass: greenwheelsConfig.carClass
+      };
 
-      $scope.feeDay = 59;
-      $scope.feeHour = 14.9;
-      $scope.feeMinute = 0.29;
-
-      $scope.feeAdditionalKm = 0.29;
-
-      $scope.resolution = ['hours'];
-      $scope.resolutionTime = ['hours'];
+      $scope.resolution = ['hours', 'days', 'weeks'];
+      $scope.resolutionTime = ['hours', 'days', 'weeks'];
 
       //-----------------------------------------------------------------------
       // convert dates and minutes, hours, weeks into durations
@@ -40,108 +43,240 @@ angular.module('c2gyoApp')
       //-----------------------------------------------------------------------
       // get actual time
       //-----------------------------------------------------------------------
-      $scope.getMinutes = function() {
-        return durationAll().minutes();
-      };
-
       $scope.getHours = function() {
         return durationAll().hours();
       };
 
       $scope.getDays = function() {
-        return Math.floor(durationAll().asDays());
+        return Math.floor(durationAll().asDays() % 7);
+      };
+
+      $scope.getWeeks = function() {
+        return Math.floor(durationAll().asDays() / 7);
       };
 
       //-----------------------------------------------------------------------
       // get billed time
       //-----------------------------------------------------------------------
-      $scope.getMinutesBilled = function() {
-        return getDurationBilled().minutes();
-      };
-
       $scope.getHoursBilled = function() {
         return getDurationBilled().hours();
       };
 
       $scope.getDaysBilled = function() {
-        return Math.floor(getDurationBilled().asDays());
+        return Math.floor(getDurationBilled().asDays() % 7);
       };
 
+      $scope.getWeeksBilled = function() {
+        return Math.floor(getDurationBilled().asDays() / 7);
+      };
+
+      //-----------------------------------------------------------------------
+      // calculate billed time
+      //-----------------------------------------------------------------------
       var getDurationBilled = function() {
-        var feeMinutes = $scope.getMinutes() * $scope.feeMinute;
-        var feeHours = $scope.getHours() * $scope.feeHour;
-        var feeDays = $scope.getDays() * $scope.feeDay;
+        var durationBilled;
 
-        var minutesBilled = $scope.getMinutes();
-        var hoursBilled = $scope.getHours();
-        var daysBilled = $scope.getDays();
-
-        if (feeMinutes >= $scope.feeHour) {
-          minutesBilled = 0;
-          feeMinutes = 0;
-          hoursBilled += 1;
-          feeHours = hoursBilled * $scope.feeHour;
+        if ($scope.rental.tab === 'simple') {
+          durationBilled = getDurationBilledSimple();
+        } else {
+          durationBilled = getDurationBilledExact();
         }
 
-        if (feeMinutes + feeHours >= $scope.feeDay) {
-          minutesBilled = 0;
-          feeMinutes = 0;
+        return durationBilled;
+      };
+
+      var getDurationBilledSimple = function() {
+        var rate = getCurrentRate().time;
+
+        var feeHours = $scope.getHours() * rate.hour;
+        var feeDays = $scope.getDays() * rate.day;
+
+        var hoursBilled = $scope.getHours();
+        var daysBilled = $scope.getDays();
+        var weeksBilled = $scope.getWeeks();
+
+        if (feeHours >= rate.day) {
           hoursBilled = 0;
           feeHours = 0;
           daysBilled += 1;
-          feeDays = daysBilled * $scope.feeDay;
+          feeDays = daysBilled * rate.day;
+        }
+
+        if (feeHours + feeDays >= rate.week) {
+          hoursBilled = 0;
+          daysBilled = 0;
+          weeksBilled += 1;
         }
 
         var duration = moment.duration({
-          minutes: minutesBilled,
           hours: hoursBilled,
           days: daysBilled,
+          weeks: weeksBilled
         });
 
         return duration;
       };
 
+      var getDurationBilledExact = function() {
+        return getDurationAndFeeExact().duration;
+      };
+
       //-----------------------------------------------------------------------
       // get current rate
       //-----------------------------------------------------------------------
+      var getCurrentRate = function() {
+        var carClass = $scope.rate.carClass;
+        //debugger;
+        return greenwheelsrate[carClass];
+      };
 
       //-----------------------------------------------------------------------
       // get price for used time
       //-----------------------------------------------------------------------
       $scope.getFeeTime = function() {
-        var feeMinutes = $scope.getMinutesBilled() * $scope.feeMinute;
-        var feeHours = $scope.getHoursBilled() * $scope.feeHour;
-        var feeDays = $scope.getDaysBilled() * $scope.feeDay;
-        var feeStanding = $scope.getFeeStanding();
+        var fee;
 
-        var fee = feeMinutes + feeHours + feeDays + feeStanding;
+        if ($scope.rental.tab === 'simple') {
+          fee = getFeeTimeSimple();
+        } else {
+          fee = getFeeTimeExact();
+        }
+
         return fee;
+      };
+
+      var getFeeTimeSimple = function() {
+        var rate = getCurrentRate().time;
+
+        var feeHours = $scope.getHoursBilled() * rate.hour;
+        var feeDays = $scope.getDaysBilled() * rate.day;
+        var feeWeeks = $scope.getWeeksBilled() * rate.week;
+
+        var fee = feeHours + feeDays + feeWeeks;
+
+        return fee;
+      };
+
+      var getFeeTimeExact = function() {
+        return getDurationAndFeeExact().fee;
+      };
+
+      //-----------------------------------------------------------------------
+      // get duration and fee exact
+      //-----------------------------------------------------------------------
+      var getDurationAndFeeExact = function() {
+
+        // init variables for calculating fee
+        var totalFeeHours = 0;
+        var totalFeeDays = 0;
+        var totalFeeWeeks = 0;
+        var rate = getCurrentRate().time;
+        var feeDay = rate.day;
+        var feeWeek = rate.week;
+
+        // init variables for billed time
+        var hoursBilled = 0;
+        var daysBilled = 0;
+        var weeksBilled = 0;
+
+        var startDate = new moment($scope.rental.startDate);
+        var endDate = new moment($scope.rental.endDate);
+        var currentTime = startDate.clone();
+
+        // go through with weeks
+        while (currentTime.clone().add(1, 'w') < endDate) {
+          totalFeeWeeks += feeWeek;
+          currentTime.add(1, 'w');
+          weeksBilled++;
+        }
+
+        // go through with days
+        while (currentTime.clone().add(1, 'd') < endDate) {
+          totalFeeDays += feeDay;
+          currentTime.add(1, 'd');
+          daysBilled++;
+        }
+
+        // go through hours exactly until endate - 1 hour
+        for (var i = currentTime.clone(); i < endDate; i.add(1, 'h')) {
+          var day = i.isoWeekday();
+          var hour = i.hour();
+          var currentRate = rate[day];
+
+          // loop through possible hour rates
+          for (var j = 0; j <= currentRate.length - 1; j++) {
+            var start = currentRate[j].start;
+            var end = currentRate[j].end;
+            var fee = currentRate[j].fee;
+
+            if (hour >= start && hour <= end) {
+              //console.log('Day: ' + day + ' hour: ' + hour + ' fee: ' + fee);
+              totalFeeHours += fee;
+              hoursBilled++;
+            }
+          }
+        }
+
+        // check to see if it is cheaper to rent for the full day
+        if (totalFeeHours >= feeDay) {
+          totalFeeHours = feeDay;
+          hoursBilled = 0;
+          daysBilled++;
+        }
+
+        // check to see if it is cheaper to rent for the full week
+        if (totalFeeDays + totalFeeHours >= feeWeek) {
+          totalFeeHours = 0;
+          totalFeeDays = feeWeek;
+          hoursBilled = 0;
+          daysBilled = 0;
+          weeksBilled++;
+        }
+
+        var duration = moment.duration({
+          hours: hoursBilled,
+          days: daysBilled,
+          weeks: weeksBilled
+        });
+
+        // fee billed
+        var totalFee = totalFeeDays + totalFeeHours + totalFeeWeeks;
+
+        return {
+          duration: duration,
+          fee: totalFee
+        };
       };
 
       //-----------------------------------------------------------------------
       // get price for distance
       //-----------------------------------------------------------------------
       $scope.getFeeDistance = function() {
-        var fee = ($scope.rental.distance - 50) * $scope.feeAdditionalKm;
-        if (fee < 0) {
-          fee = 0;
-        }
-        return fee;
-      };
+        var rate = getCurrentRate().km;
+        var km = $scope.rental.distance;
+        var totalFee = 0;
 
-      //-----------------------------------------------------------------------
-      // get other fees
-      //-----------------------------------------------------------------------
-      $scope.getFeeStanding = function() {
-        return $scope.rental.timeStanding * 0.19;
-      };
+        // counter for possible km rates
+        var k = 0;
 
-      $scope.getFeeAirport = function() {
-        var fee = 0;
-        if ($scope.rental.airport) {
-          fee = 4.90;
+        // go through kms
+        while (km > 0) {
+          var start = rate[k].start;
+          var end = rate[k].end;
+          var fee = rate[k].fee;
+          var range = end - start;
+
+          if (km <= range || end === -1) {
+            totalFee += (km * fee);
+            km = 0;
+          } else {
+            km = km - range;
+            totalFee += (range * fee);
+            k++;
+          }
         }
-        return fee;
+
+        return totalFee;
       };
 
       //-----------------------------------------------------------------------
@@ -150,9 +285,9 @@ angular.module('c2gyoApp')
       $scope.price = function() {
         return (
         $scope.getFeeTime() +
-        $scope.getFeeDistance() +
-        $scope.getFeeAirport()
+        $scope.getFeeDistance()
         );
       };
+
     }
   ]);
---
+++ app/scripts/services/greenwheelsconfig.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name c2gyoApp.smConfig
+ * @description
+ * # smConfig
+ * Constant in the c2gyoApp.
+ */
+angular.module('c2gyoApp')
+  .constant('greenwheelsConfig', {
+    carClass: 'compact'
+  });
