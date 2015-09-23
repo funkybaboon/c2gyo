e29fff45316e9c750d5d57b9d5c373d5b71659ee
Benjamin <funkybaboon@gmail.com>
2015-09-23 09:16:15 +0200

    remove old tests, activate new tests in protractor con

--- test/protractor/protractor-conf.js
+++ test/protractor/protractor-conf.js
@@ -1,38 +1,24 @@
 exports.config = {
   seleniumAddress: 'http://127.0.0.1:4444/wd/hub',
 
-  specs: [/*
-    'c2g10km20minScenario.js',
-    'c2g10km229minScenario.js',
-    'c2g10km2000minScenario.js',
-    'c2g100km20minScenario.js',
-    'c2g1000km2000minScenario.js',
-
-    'c2gb10km1hourScenario.js',
-    'c2gb10km33hoursScenario.js',
-    'c2gb100km1hourScenario.js',
-    'c2gb1000km33hoursScenario.js',
-
-    'sm10km10hoursClassicScenario.js',
-    'sm10km10hoursBasicScenario.js',
-    'sm10km10hoursBusinessScenario.js',
-*/
+  specs: [
     'KeepStateScenario.js',
 
     'c2g.js',
     'c2gb.js',
     'flinkster.js',
-    'greenwheels.js'
+    'greenwheels.js',
+    'stadtmobil.js'
   ],
 
   multiCapabilities: [{
     browserName: 'chrome',
     shardTestFiles: true,
     maxInstances: 99
-  }, {
+  }/*, {
     browserName: 'firefox',
     shardTestFiles: true,
     maxInstances: 99
-  }]
+  }*/]
 
 };
