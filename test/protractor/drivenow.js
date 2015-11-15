33a6e9d654904ca788d56686b91d78d235d8be90
Benjamin <funkybaboon@gmail.com>
2015-11-16 00:09:07 +0100

    add test for drivenow week input field

--- test/protractor/drivenow.js
+++ test/protractor/drivenow.js
@@ -11,9 +11,17 @@ describe('drivenow test all input fields and checkboxes', function() {
 
     element(by.model('rental.timeHours')).clear().sendKeys(2);
     element(by.model('rental.timeDays')).clear().sendKeys(2);
-    element(by.model('rental.timeWeeks')).clear().sendKeys(2);
     element(by.model('rental.distance')).clear().sendKeys(222);
 
+  });
+
+  it('should display the week input field if rental time is greater than 7 ' +
+  'days', function() {
+    expect(element(by.model('rental.timeWeeks')).isPresent()).toBe(false);
+    element(by.model('rental.timeDays')).clear().sendKeys(8);
+    expect(element(by.model('rental.timeWeeks')).isPresent()).toBe(true);
+    element(by.model('rental.timeDays')).clear().sendKeys(6);
+    expect(element(by.model('rental.timeWeeks')).isPresent()).toBe(false);
   });
 
   var popover = [
