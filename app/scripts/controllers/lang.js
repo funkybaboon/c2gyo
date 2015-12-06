0c740313363f9c2edde4bf36a2cbc56f327141a4
Benjamin <funkybaboon@gmail.com>
2015-12-06 17:55:56 +0100

    add links to switch between de and en language

---
+++ app/scripts/controllers/lang.js
@@ -0,0 +1,20 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name c2gyoApp.controller:LangCtrl
+ * @description
+ * # LangCtrl
+ * Controller of the c2gyoApp
+ */
+angular.module('c2gyoApp')
+  .controller('LangCtrl', function ($scope, $translate) {
+
+  $scope.changeLang = function (key) {
+    $translate.use(key).then(function (key) {
+      console.log("Sprache zu " + key + " gewechselt.");
+    }, function (key) {
+      console.log("Irgendwas lief schief.");
+    });
+  };
+});
