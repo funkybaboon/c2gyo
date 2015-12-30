31ca05a9ea65d002ae53ce8662d81ebc25fc09a5
Benjamin <funkybaboon@gmail.com>
2015-12-30 13:08:12 +0100

    remove redundant code

--- app/scripts/directives/popover.js
+++ app/scripts/directives/popover.js
@@ -9,32 +9,35 @@
  <span tariff-popover></span>
  */
 angular.module('c2gyoApp')
-  .directive('tariffPopover', ['$translate', '$rootScope', function ($translate, $rootScope) {
-    return {
-      restrict: 'A',
-      transclude: true,
-      scope: {
-        translateText: '@tariffPopover'
-      },
-      template:
-        '<span ng-transclude></span>' +
-        '&nbsp;' +
-        '<span popover-placement="right" ' +
-        '      uib-popover-html="text" ' +
-        '      popover-trigger="mouseenter" ' +
-        '      class="fa fa-info-circle">' +
-        '</span>',
-      controller: function ($scope) {
-        $translate($scope.translateText).then(function (translatedText) {
-          $scope.text = translatedText;
-        });
+  .directive('tariffPopover', [
+    '$translate',
+    '$rootScope',
+    function ($translate, $rootScope) {
+      return {
+        restrict: 'A',
+        transclude: true,
+        scope: {
+          translateText: '@tariffPopover'
+        },
+        template:
+          '<span ng-transclude></span>' +
+          '&nbsp;' +
+          '<span popover-placement="right" ' +
+          '      uib-popover-html="text" ' +
+          '      popover-trigger="mouseenter" ' +
+          '      class="fa fa-info-circle">' +
+          '</span>',
+        controller: function ($scope) {
+          var translate = function(){
+            $translate($scope.translateText).then(function (translatedText) {
+              $scope.text = translatedText;
+            });
+          }
 
-        $rootScope.$on('$translateChangeSuccess', function () {
-          $translate($scope.translateText).then(function (translatedText) {
-            $scope.text = translatedText;
-          });
-        });
-
-      }
-    };
-  }]);
+          translate();
+          $rootScope.$on('$translateChangeSuccess', translate);
+        }
+      };
+    }
+  ]
+);
