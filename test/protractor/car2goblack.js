ccdc76d354f72866de252341861f3b5699ecb16f
Benjamin <funkybaboon@gmail.com>
2016-01-03 00:27:40 +0100

    fix car2goblack tests

--- test/protractor/car2goblack.js
+++ test/protractor/car2goblack.js
@@ -56,16 +56,16 @@ describe('car2goblack test all input fields and checkboxes', function() {
   });
 
   it('should display the popover-content on mouseover', function() {
-    var pathIcon = 'span[tariff-popover="views/popovers/car2goblack/airport.html"]' +
-      ' > .fa.fa-info-circle';
-    var pathPopover = 'span[tariff-popover="views/popovers/car2goblack/airport.html"] ' +
-      '> .popover.ng-isolate-scope.right.fade.in';
+    var path = 'span[tariff-popover="popover.car2goblack.airport"]';
+    var pathIcon =  path + ' > .fa.fa-info-circle';
+    var pathPopover = path + '> .popover.ng-isolate-scope.right.fade.in';
 
     var popoverIcon = element(by.css(pathIcon));
     browser.actions().mouseMove(popoverIcon).perform();
     var popover = element(by.css(pathPopover));
 
     expect(popover.isDisplayed()).toBeTruthy();
+    browser.sleep(browser.params.sleepTimeout);
     expect(popover.getText()).toMatch(browser.params.regexNotEmpty);
   });
 });
