4a4742b84e4b28d46a565a046c7b14ca171730a5
Benjamin <funkybaboon@gmail.com>
2016-02-04 13:00:26 +0100

    add bookndrive rate abo

---
+++ app/scripts/services/bookndriverateabo.js
@@ -0,0 +1,124 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name c2gyoApp.bookndriverateabo
+ * @description
+ * # bookndriverateabo
+ * Constant in the c2gyoApp.
+ */
+angular.module('c2gyoApp')
+  .constant('bookndriverateabo', {
+    'resolution': 'h',
+    'cityflitzer': {
+      'time': {
+        '1': [
+          {'start': 0, 'end': 23, 'fee': 1.00}],
+        '2': [
+          {'start': 0, 'end': 23, 'fee': 1.00}],
+        '3': [
+          {'start': 0, 'end': 23, 'fee': 1.00}],
+        '4':  [
+          {'start': 0, 'end': 23, 'fee': 1.00}],
+        '5':  [
+          {'start': 0, 'end': 23, 'fee': 1.00}],
+        '6':  [
+          {'start': 0, 'end': 23, 'fee': 1.00}],
+        '7': [
+          {'start': 0, 'end': 23, 'fee': 1.00}],
+        'hour': 1.00,
+        'day': 24.00
+     },
+      'km': [
+        {'start': 0, 'end': -1, 'fee': 0.15}],
+   },
+    'xs': {
+      'time': {
+        '1': [
+          {'start': 0, 'end': 23, 'fee': 1.50}],
+        '2': [
+          {'start': 0, 'end': 23, 'fee': 1.50}],
+        '3': [
+          {'start': 0, 'end': 23, 'fee': 1.50}],
+        '4':  [
+          {'start': 0, 'end': 23, 'fee': 1.50}],
+        '5':  [
+          {'start': 0, 'end': 23, 'fee': 1.50}],
+        '6':  [
+          {'start': 0, 'end': 23, 'fee': 1.50}],
+        '7': [
+          {'start': 0, 'end': 23, 'fee': 1.50}],
+        'hour': 1.50,
+        'day': 15.00
+     },
+      'km': [
+        {'start': 0, 'end': -1, 'fee': 0.15}],
+   },
+    's': {
+      'time': {
+        '1': [
+          {'start': 0, 'end': 23, 'fee': 1.90}],
+        '2': [
+          {'start': 0, 'end': 23, 'fee': 1.90}],
+        '3': [
+          {'start': 0, 'end': 23, 'fee': 1.90}],
+        '4':  [
+          {'start': 0, 'end': 23, 'fee': 1.90}],
+        '5':  [
+          {'start': 0, 'end': 23, 'fee': 1.90}],
+        '6':  [
+          {'start': 0, 'end': 23, 'fee': 1.90}],
+        '7': [
+          {'start': 0, 'end': 23, 'fee': 1.90}],
+        'hour': 1.90,
+        'day': 19.00
+     },
+      'km': [
+        {'start': 0, 'end': -1, 'fee': 0.17}],
+   },
+    'm': {
+      'time': {
+        '1': [
+          {'start': 0, 'end': 23, 'fee': 2.50}],
+        '2': [
+          {'start': 0, 'end': 23, 'fee': 2.50}],
+        '3': [
+          {'start': 0, 'end': 23, 'fee': 2.50}],
+        '4':  [
+          {'start': 0, 'end': 23, 'fee': 2.50}],
+        '5':  [
+          {'start': 0, 'end': 23, 'fee': 2.50}],
+        '6':  [
+          {'start': 0, 'end': 23, 'fee': 2.50}],
+        '7': [
+          {'start': 0, 'end': 23, 'fee': 2.50}],
+        'hour': 2.50,
+        'day': 25.00
+     },
+      'km': [
+        {'start': 0, 'end': -1, 'fee': 0.19}],
+   },
+    'l': {
+      'time': {
+        '1': [
+          {'start': 0, 'end': 23, 'fee': 3.90}],
+        '2': [
+          {'start': 0, 'end': 23, 'fee': 3.90}],
+        '3': [
+          {'start': 0, 'end': 23, 'fee': 3.90}],
+        '4':  [
+          {'start': 0, 'end': 23, 'fee': 3.90}],
+        '5':  [
+          {'start': 0, 'end': 23, 'fee': 3.90}],
+        '6':  [
+          {'start': 0, 'end': 23, 'fee': 3.90}],
+        '7': [
+          {'start': 0, 'end': 23, 'fee': 3.90}],
+        'hour': 3.90,
+        'day': 39.00
+     },
+      'km': [
+        {'start': 0, 'end': -1, 'fee': 0.22}],
+   }
+ }
+);
