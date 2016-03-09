163126122edd6eb8404d1a177f99fe6b63334388
Benjamin <funkybaboon@gmail.com>
2016-03-09 13:02:31 +0100

    add car class form directive

---
+++ app/scripts/directives/carclassform.js
@@ -0,0 +1,31 @@
+'use strict';
+
+/**
+ * @ngdoc directive
+ * @name c2gyoApp.directive:formWithPopover
+ * @description
+ * # popover
+ * @example
+ <span tariff-popover></span>
+ */
+angular.module('c2gyoApp')
+  .directive('carClassForm', [
+      '$translate',
+      '$rootScope',
+      'state',
+      function ($translate, $rootScope, state) {
+        return {
+          restrict: 'E',
+          replace: 'true',
+          templateUrl: 'views/carclassform.html',
+          scope: {
+            options: '=listofoptions',
+            vendor: '@'
+          },
+          controller: function ($scope) {
+            $scope.rental = state.rental[$scope.vendor];
+          }
+        };
+      }
+    ]
+  );
