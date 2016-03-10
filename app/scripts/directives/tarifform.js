3881f4391681fbbee92284548c8bada617b99b8c
Benjamin <funkybaboon@gmail.com>
2016-03-10 13:00:25 +0100

    add tariff form directive

---
+++ app/scripts/directives/tarifform.js
@@ -0,0 +1,31 @@
+'use strict';
+
+/**
+ * @ngdoc directive
+ * @name c2gyoApp.directive:formWithPopover
+ * @description
+ * # popover
+ * @example
+ <span tariff-popover></span>
+ */
+angular.module('c2gyoApp')
+  .directive('tarifForm', [
+      '$translate',
+      '$rootScope',
+      'state',
+      function ($translate, $rootScope, state) {
+        return {
+          restrict: 'E',
+          replace: 'true',
+          templateUrl: 'views/tarifform.html',
+          scope: {
+            options: '=listofoptions',
+            vendor: '@'
+          },
+          controller: function ($scope) {
+            $scope.rental = state.rental[$scope.vendor];
+          }
+        };
+      }
+    ]
+  );
