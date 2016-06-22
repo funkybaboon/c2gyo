8a1ca0e90e9f931bb229570ca8de0139cf811d85
Benjamin <funkybaboon@gmail.com>
2016-06-22 23:38:24 +0200

    use new tarif form in view

--- test/protractor/stadtmobil.js
+++ test/protractor/stadtmobil.js
@@ -24,42 +24,42 @@ describe('test stadtmobil input fields', function() {
   });
 
   it('should calculate a price with all checkboxes ticked', function() {
-    element(by.id('tariffclassic')).click();
+    element(by.id('tariff.classic')).click();
     expect(price.getText()).toEqual('336,76 €');
     expect(priceDistance.getText()).toEqual('41,96 €');
     expect(priceTime.getText()).toEqual('294,80 €');
 
-    element(by.id('tariffbasic')).click();
+    element(by.id('tariff.basic')).click();
     expect(price.getText()).toEqual('379,16 €');
     expect(priceDistance.getText()).toEqual('43,96 €');
     expect(priceTime.getText()).toEqual('335,20 €');
 
-    element(by.id('tariffstudi')).click();
+    element(by.id('tariff.studi')).click();
     expect(price.getText()).toEqual('336,76 €');
     expect(priceDistance.getText()).toEqual('41,96 €');
     expect(priceTime.getText()).toEqual('294,80 €');
 
-    element(by.id('tariffbusiness')).click();
+    element(by.id('tariff.business')).click();
     expect(price.getText()).toEqual('310,34 €');
     expect(priceDistance.getText()).toEqual('37,74 €');
     expect(priceTime.getText()).toEqual('272,60 €');
 
-    element(by.id('rateB')).click();
+    element(by.id('carclass.b')).click();
     expect(price.getText()).toEqual('335,54 €');
     expect(priceDistance.getText()).toEqual('37,74 €');
     expect(priceTime.getText()).toEqual('297,80 €');
 
-    element(by.id('rateC')).click();
+    element(by.id('carclass.c')).click();
     expect(price.getText()).toEqual('390,56 €');
     expect(priceDistance.getText()).toEqual('39,96 €');
     expect(priceTime.getText()).toEqual('350,60 €');
 
-    element(by.id('rateD')).click();
+    element(by.id('carclass.d')).click();
     expect(price.getText()).toEqual('475,50 €');
     expect(priceDistance.getText()).toEqual('55,50 €');
     expect(priceTime.getText()).toEqual('420,00 €');
 
-    element(by.id('rateF')).click();
+    element(by.id('carclass.f')).click();
     expect(price.getText()).toEqual('607,54 €');
     expect(priceDistance.getText()).toEqual('59,94 €');
     expect(priceTime.getText()).toEqual('547,60 €');
